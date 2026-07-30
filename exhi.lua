1c8cce9fc868b93856f64fb39ca397d608cdb647
maygods <funniman699@gmail.com>
2026-07-30 06:30:38 +0100

    Update exhi.lua

--- exhi.lua
+++ exhi.lua
@@ -994,7 +994,7 @@ function ExhibitionLib:CreateWindow(cfg)
                 if cSize % 2 == 0 then cSize = cSize + 1 end
                 local cOff = math.floor(cSize / 2)
                 local edgeOffset = 4 * GUI_SCALE -- Section padding
-                local gap = math.floor(3 * GUI_SCALE) -- Visual gap between buttons and track (increased for more space)
+                local gap = math.floor(1 * GUI_SCALE) -- Small visual gap to keep the slider track as long as possible
 
                 local trackOut = Create("Frame", {
                     BackgroundColor3 = ThemeColor("GroupBorderOut"),
