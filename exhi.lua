aaf2c3f0872fd1c4f7e58a4c596918d0049575a9
maygods <funniman699@gmail.com>
2026-07-30 03:14:55 +0100

    Update exhi.lua

--- exhi.lua
+++ exhi.lua
@@ -903,7 +903,7 @@ function ExhibitionLib:CreateWindow(cfg)
                 MountComponent(wrap, ecfg.HalfSize)
                 
                 local nameLbl = DrawTextWithShadow(wrap, Capitalize(ecfg.Name or "Slider"), Fonts.Regular, 9 * GUI_SCALE, Colors.TextDim, UDim2.new(0, 0, 0, 0), Enum.TextXAlignment.Left, 2)
-                nameLbl.Size = UDim2.new(1, -42 * GUI_SCALE, 0, 9 * GUI_SCALE)
+                nameLbl.Size = UDim2.new(1, -15 * GUI_SCALE, 0, 9 * GUI_SCALE)
                 nameLbl.TextTruncate = Enum.TextTruncate.AtEnd
                 
                 local trackOut = Create("Frame", {
